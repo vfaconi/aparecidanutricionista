065e6d4f6f39ae8ccf358da21ff1775fa8e5e739
vfaconi <38166675+vfaconi@users.noreply.github.com>
2020-01-08 23:10:07 -0300

    Add files via upload

---
+++ introducao-javascript/js/buscar-pacientes.js
@@ -0,0 +1,21 @@
+var botaoAdicionar = document.querySelector("#buscar-paciente");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var resposta = xhr.responseText;
+
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
+    });
+
+    xhr.send();
+});
